16a37c4ee07c7202c89d6e23ddffdacec7895f82
rhysshaiqal <124010137+rhysshaiqal@users.noreply.github.com>
2024-04-22 23:21:03 -0400

    Delete user_interface_module.py

--- user_interface_module.py
+++
@@ -1,7 +0,0 @@
-import argparse
-
-def parse_arguments():
-    parser = argparse.ArgumentParser(description='Real-Time Weather Data Visualization Tool')
-    parser.add_argument('--location', type=str, help='Location to fetch weather for')
-    args = parser.parse_args()
-    return args
